cb35edaea9b18c79f9c31ef140bd726cc841e0ab
Michael Fester <michael.fester@gmail.com>
2017-07-27 19:41:48 +0200

    Fix tests

--- tests/test_skill.py
+++ tests/test_skill.py
@@ -3,22 +3,12 @@ from unittest import TestCase
 from snipshue.snipshue import SnipsHue
 
 
-class HueConfig():
-
-    def __init__(self, hostname, username, light_ids):
-        self.hostname = hostname
-        self.username = username
-        self.light_ids = light_ids
-
-
 class TestSkill(TestCase):
 
     def setUp(self):
-        config = HueConfig("192.168.163.96",
-                           "XyWT7TlG3RkMAzbKRt4fWpHEo1TudukIgH2OKz79",
-                           []
-                           )
-        self.skill = SnipsHue(config)
+        self.skill = SnipsHue("192.168.163.96",
+                              "XyWT7TlG3RkMAzbKRt4fWpHEo1TudukIgH2OKz79",
+                              [])
 
     def test_skill(self):
         self.skill.turn_on()
