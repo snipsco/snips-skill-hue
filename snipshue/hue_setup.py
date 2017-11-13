443448952f57c05fdc032901b32bce48a929e7d1
Michael Fester <michael.fester@gmail.com>
2017-11-13 18:44:24 +0100

    Temp fix for discover

---
+++ snipshue/hue_setup.py
@@ -0,0 +1,114 @@
+import requests
+import json
+import os
+import errno
+import sys
+
+cache_file_name = 'credentials.json'
+path_cache_folder = os.path.expanduser('~/.snips/philips_hue/')
+
+class HueSetup:
+    """ Get or create infos to connect to Philips Hue """
+
+    bridge_url = None
+
+    def __init__(self):
+        bridge_ip = self._get_bridge_ip()
+        username = self._get_cached_username(bridge_ip)
+
+        print "Cached username: " + str(username)
+
+        if username is None or not self._is_connected(bridge_ip, username):
+            username = self._connect_user(bridge_ip)
+
+        self.bridge_url = self._create_url(bridge_ip, username)
+
+        self._set_cache(bridge_ip, username)
+
+    def _get_bridge_ip(self):
+        response = requests.get('http://www.meethue.com/api/nupnp').json()
+        bridge_ip = response[0]["internalipaddress"] if type(response) is list else response["internalipaddress"]
+
+        return bridge_ip
+
+    def _is_connected(self, bridge_ip, username):
+        is_connected = False
+        resource = {'which':'system'}
+        response = requests.get(self._create_url(bridge_ip, username)).json()
+
+        if 'lights' in response:
+            print 'Connected to the Hub'
+            is_connected = True
+        elif 'error' in response[0]:
+            error = response[0]['error']
+            if error['type'] == 1:
+                is_connected = False
+
+        return is_connected
+
+    def _get_cached_username(self, bridge_ip):
+        username = None
+        try:
+            f = open(path_cache_folder + cache_file_name, 'rw')
+
+            content = f.read()
+            try:
+                content = json.loads(content)
+            except ValueError, e:
+                content = {}
+
+            username = content.get(bridge_ip)
+            f.close()
+        except IOError:
+            print "No cached data were found" 
+        return username
+
+    def _create_file_if_not_exist(self, path, filename):
+        try:
+            os.makedirs(path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+
+        open(path + filename, 'w+').close()
+
+    def _set_cache(self, bridge_ip, username):
+        self._create_file_if_not_exist(path_cache_folder, cache_file_name)
+        f = open(path_cache_folder + cache_file_name, 'r+')
+        content = f.read()
+
+        if type(content) == dict:
+            content = json.loads(content)
+        else:
+            content = {}
+
+        content[bridge_ip] = username
+
+        content = json.dumps(content)
+        f.write(content)
+        f.close()
+
+    def _connect_user(self, bridge_ip):
+        created = False
+
+        print '/!\ Please, press the button on the Hue bridge'
+
+        while not created:
+            payload = json.dumps({'devicetype': 'snipshue'})
+            # response = requests.Post(resource)['resource']
+            response = requests.post("http://" + bridge_ip + "/api", data=payload).json()
+            if 'error' in response[0]:
+                if response[0]['error']['type'] != 101:
+                    print 'Unhandled error creating configuration on the Hue'
+                    sys.exit(response)
+            else:
+                username = response[0]['success']['username']
+                created = True
+
+        print 'User connected'
+
+        return (username)
+
+
+    def _create_url(self, bridge_ip, username):
+        return 'http://{}/api/{}'.format(bridge_ip, username)
--- snipshue/snipshue.py
+++ snipshue/snipshue.py
@@ -5,64 +5,111 @@ import requests
 import json
 import time
 import os
+import errno
+import sys
+import hue_setup
 
+from color_utils import colors
 
 class SnipsHue:
     """ Philips Hue skill for Snips. """
 
-    def __init__(self, hostname, username, light_ids):
-        """
+    colormap = colors
+
+    def __init__(self, hostname=None, username=None):
+        """ Initialisation.
+
         :param hostname: Philips Hue hostname
         :param username: Philips Hue username
         :param light_ids: Philips Hue light ids
         """
-        self.endpoint = 'http://{}/api/{}/lights'.format(
-            hostname, username)
-        self.light_ids = light_ids        
+        if hostname is None or username is None:
+            setup = hue_setup.HueSetup()
+            url = setup.bridge_url
 
-    def turn_on(self):
-        """ Turn on all Philips Hue lights. """
-        self.post_state_all({"on": True})
+            print str(url)
+        else:
+            url = 'http://{}/api/{}'.format(hostname, username)
 
-    def turn_off(self):
+        self.lights_endpoint = url + "/lights"
+        self.groups_endpoint = url + "/groups"
+        self.config_endpoint = url + "/config"
+
+        self.lights_from_room = self._get_rooms_lights()
+
+    def light_on_set(self, color, intensity, location):
+        """ Turn on Philips Hue lights in [location] at [intensity] with [color] color. """
+
+        light_ids = self._get_light_ids_from_room(location)
+
+        state = {"on": True}
+        if intensity != None:
+            state.update({"bri": intensity})
+        if color != None:
+            state.update(self._get_hue_saturation(color))
+
+        self._post_state_to_ids(state, light_ids)
+
+    def light_off(self, location):
         """ Turn off all Philips Hue lights. """
-        self.post_state_all({"on": False})
+        self._post_state_to_ids({"on": False}, self._get_light_ids_from_room(location))
 
-    def set_color_name(self, color):
-        """ Set the light color for all lights.
+    def light_up(self, intensity_augmentation, location):
+        """ Increase Philips Hue lights' intensity. """
+        light_ids = self._get_light_ids_from_room(location)
 
-        :param color: A color name, e.g. "red" or "blue"
-        """
-        params = self.get_HSR(color)
-        params["on"] = True
-        self.post_state_all(params)
+        lights_config = self._get_lights_config(light_ids)
 
-    def post_state_all(self, params):
-        """ Post a state update to all Philips Hue lights.
+        for light_id in light_ids:
+            intensity = lights_config[light_id]["bri"]
+            if intensity + intensity_augmentation > 254:
+                intensity = 254
+            else:
+                intensity += intensity_augmentation
+            self._post_state({"on": True, "bri": intensity}, light_id)
 
-        :param params: Philips Hue request parameters.
-        """
-        p = dict((k, v) for k, v in params.items() if v != None)
-        for light_id in self.light_ids:
-            self.post_state(p, light_id)
-            time.sleep(0.2)
+    def light_down(self, intensity_reduction, location):
+        """ Lower Philips Hue lights' intensity. """
+        light_ids = self._get_light_ids_from_room(location)
 
-    def post_state(self, params, light_id):
+        lights_config = self._get_lights_config(light_ids)
+
+        for light_id in light_ids:
+            intensity = lights_config[light_id]["bri"]
+            if intensity < intensity_reduction:
+                intensity = 0
+            else:
+                intensity -= intensity_reduction
+            self._post_state({"bri": intensity}, light_id)
+
+    def _post_state_to_ids(self, params, light_ids):
+        """ Post a state update to specyfied Philips Hue lights. """
+        try:
+            for light_id in light_ids:
+                self._post_state(params, light_id)
+                time.sleep(0.2)
+        except Exception as e:
+            return
+
+    def _post_state(self, params, light_id):
         """ Post a state update to a given light.
 
         :param params: Philips Hue request parameters.
         :param light_id: Philips Hue light ID.
         """
+        if (light_id is None) or (params is None):
+            return
+
         print("[HUE] Setting state for light " +
               str(light_id) + ": " + str(params))
         try:
-            url = "{}/{}/state".format(self.endpoint, light_id)
+            url = "{}/{}/state".format(self.lights_endpoint, light_id)
             requests.put(url, data=json.dumps(params), headers=None)
         except:
             print("[HUE] Request timeout. Is the Hue Bridge reachable?")
             pass
 
-    def get_HSR(self, color_name):
+    def _get_hue_saturation(self, color_name):
         """ Transform a color name into a dictionary represenation
             of a color, as expected by the Philips Hue API.
 
@@ -70,10 +117,46 @@ class SnipsHue:
         :return: A dictionary represenation of a color, as expected
             by the Philips Hue API, e.g. {'hue': 65535, 'sat': 254, 'bri': 254}
         """
-        if color_name == 'red':
-            return {'hue': 65535, 'sat': 254, 'bri': 254}
-        elif color_name == 'green':
-            return {'hue': 25500, 'sat': 254, 'bri': 254}
-        elif color_name == 'blue':
-            return {'hue': 46920, 'sat': 254, 'bri': 254}
-        return {'hue': 0, 'sat': 0, 'bri': 254}
+        return self.colormap.get(color_name, {'hue': 0, 'sat': 0})
+
+    def _get_lights_config(self, light_ids):
+        """ Make a get request to get infos about the current state of the given lights """
+        lights = {}
+
+        for light_id in light_ids:
+            current_config = requests.get(self.lights_endpoint + "/" + str(light_id)).json()["state"]
+            lights[light_id] = current_config
+
+        return lights
+
+    def _get_all_lights(self):
+        lights = requests.get(self.lights_endpoint).json()
+        return lights.keys()
+
+    def _get_light_ids_from_room(self, room):
+        """ Returns the list of lights in a [room] or all light_ids if [room] is None """
+        if (room == None or self.lights_from_room.get(room) == None):
+            return self._get_all_lights()
+
+        return self.lights_from_room[room]
+
+    def _get_rooms_lights(self):
+        """ Returns a dict {"room_name": {"light1", "light2"}, ...} after calling the Hue API """
+        groups = requests.get(self.groups_endpoint).json()
+        ids_from_room = {}
+
+        for key, value in groups.iteritems():
+            group = value
+            if group.get("class") != None:
+                ids_from_room[group["class"]] = [ str(x) for x in group["lights"] ]
+
+        print "[HUE] Available rooms: \n" + ("\n".join(ids_from_room.keys()))
+
+        return ids_from_room
+
+
+if __name__ == "__main__":
+    sh = SnipsHue()
+    # sh.light_on_set("gold", 42, "Bedroom")
+    sh.light_on_set("red", 42, "Office")
+    print sh._get_all_lights()
